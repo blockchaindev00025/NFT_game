593e28eb4f57582e8a2c275df93162cd064d4fb8
BlockChain & FullStack Developer <95298844+CryptoDev1121@users.noreply.github.com>
2022-07-20 15:45:03 +1000

    Update index.js

--- src/views/index.js
+++ src/views/index.js
@@ -4,3 +4,5 @@ export { default as Stake } from "./Stake/Stake";
 export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as PoolTogether } from "./33Together/33Together.jsx";
 export { default as TreasuryDashboard } from "./TreasuryDashboard/TreasuryDashboard";
+
+
